12f26d41021aab9ce9bcaa731b572983380d30b8
Viccenzo de Oliveira N. Resende <128618121+oviccenzo@users.noreply.github.com>
2025-08-05 10:24:56 -0300

    Create exemplo1.cpp

---
+++ exemplo1.cpp
@@ -0,0 +1,24 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
+  Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    int m = a[0]; 
+    
+    for(int i = 0; i < m; i++){
+        if(a[i] >= m){
+            m = a[i];
+        }
+    }
+    
+    printf("Hello World");
+
+    return 0;
+}
